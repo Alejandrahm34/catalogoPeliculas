04212468d659b1f8add269a039467735e1603e78
Alejandra Herrera <alejandrahm23@gmail.com>
2024-07-17 20:18:26 -0600

    CRUD funcionando

--- backend/controllers/peliculasControllers.js
+++ backend/controllers/peliculasControllers.js
@@ -1,7 +1,22 @@
+
 const getPeliculas = (req, res) => {
     res.status(200).json({message: 'getPeliculas'});//mensaje en postman cuando ponga el metodo get
 
 }
+const crearPeliculas = (req, res) => {
+    res.status(201).json({message: 'crearPeliculas'});
+}
+
+const updatePeliculas = (req, res) => {
+    res.status(200).json({message:`Modificar la pelicula ${req.params.id}` });
+}
+
+const deletePeliculas = (req, res) => {
+    res.status(201).json({message:`Eliminar la pelicula ${req.params.id}`});
+}
 module.exports = {
-    getPeliculas
+    getPeliculas,
+    crearPeliculas,
+    updatePeliculas,
+    deletePeliculas
 }
--- backend/routes/peliculasRoutes.js
+++ backend/routes/peliculasRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getPeliculas} = require('../controllers/peliculasControllers');
+const {getPeliculas, crearPeliculas, updatePeliculas, deletePeliculas} = require('../controllers/peliculasControllers');
 
 router.get('/', getPeliculas);
+router.post('/', crearPeliculas);
+router.put('/:id', updatePeliculas);
+router.delete('/:id', deletePeliculas);
 
 module.exports = router;
